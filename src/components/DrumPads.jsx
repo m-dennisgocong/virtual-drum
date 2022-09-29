b0657a4337538eaffbd81b2952075c5306f74183
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-29 16:33:52 +0700

    add component DrumPads
    this component contain the keys/drum-pad

---
+++ src/components/DrumPads.jsx
@@ -0,0 +1,21 @@
+import './DrumPads.scss';
+const DrumPads = () => {
+
+    return(
+        <section className="drum-map">
+            <ul>
+                <li className="drum-pad">Q</li>
+                <li className="drum-pad">W</li>
+                <li className="drum-pad">E</li>
+                <li className="drum-pad">A</li>
+                <li className="drum-pad">S</li>
+                <li className="drum-pad">D</li>
+                <li className="drum-pad">Z</li>
+                <li className="drum-pad">X</li>
+                <li className="drum-pad">C</li>
+            </ul>
+        </section>
+    );
+}
+
+export default DrumPads;
