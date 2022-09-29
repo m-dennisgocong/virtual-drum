773b5400791944061ed174204524b4570c5b0b26
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-29 16:52:59 +0700

    close #3 add clickable list of drum pads
    create an unordered list with 9 elements inside, containing a clickable list of drum pad

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -1,18 +1,22 @@
 import './DrumPads.scss';
 const DrumPads = () => {
 
+    const clickPad = (event) => {
+        console.log("Click!");
+    }
+
     return(
         <section className="drum-map">
             <ul>
-                <li className="drum-pad">Q</li>
-                <li className="drum-pad">W</li>
-                <li className="drum-pad">E</li>
-                <li className="drum-pad">A</li>
-                <li className="drum-pad">S</li>
-                <li className="drum-pad">D</li>
-                <li className="drum-pad">Z</li>
-                <li className="drum-pad">X</li>
-                <li className="drum-pad">C</li>
+                <li id="Heater-1" className="drum-pad" onClick={clickPad}>Q</li>
+                <li id="Heater-2" className="drum-pad" onClick={clickPad}>W</li>
+                <li id="Heater-3" className="drum-pad" onClick={clickPad}>E</li>
+                <li id="Heater-4" className="drum-pad" onClick={clickPad}>A</li>
+                <li id="Clap" className="drum-pad" onClick={clickPad}>S</li>
+                <li id="Open-HH" className="drum-pad" onClick={clickPad}>D</li>
+                <li id="Kick-n'-Hack" className="drum-pad" onClick={clickPad}>Z</li>
+                <li id="Kick" className="drum-pad" onClick={clickPad}>X</li>
+                <li id="Closed-HH" className="drum-pad" onClick={clickPad}>C</li>
             </ul>
         </section>
     );
