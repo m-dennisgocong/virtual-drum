3e19fe1060a3342f568d7065dc518a9408fc22e5
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-29 16:27:27 +0700

    import components, DrumPads and ControlContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.scss';
-
+import DrumPads from './components/DrumPads';
+import ControlContainer from './components/ControlContainer';
 const App = () => {
 
   return(
     <main id="drum-machine" className="drum-container">
       <p id="display"></p>
+      <DrumPads />
+      <ControlContainer />
     </main>
   );
 }
