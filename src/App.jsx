d8bf7afbdda979df6ce9ab023191bb22e43873c0
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-30 13:37:18 +0700

    import bankOne and set as default current bank

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import './App.scss';
 import DrumPads from './components/DrumPads';
 import ControlContainer from './components/ControlContainer';
+import {bankOne} from './data/bankOne.js';
+
 const App = () => {
+  
+  const [currentBank, setCurrentBank] = useState(bankOne);
 
   return(
     <main id="drum-machine" className="drum-container">
