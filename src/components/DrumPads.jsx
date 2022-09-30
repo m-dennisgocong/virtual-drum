4a0f76f2518bcd2cedf8136be0c904f358762c9d
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-30 14:30:20 +0700

    use currentBank prop and remove sample list
    close #4 replace the sample list (drum-pad) with a new list element containing the audio by using map method to currentBank prop

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -1,22 +1,18 @@
 import './DrumPads.scss';
-const DrumPads = () => {
+const DrumPads = ({currentBank}) => {
 
     const clickPad = (event) => {
         console.log("Click!");
     }
-
+    
+    const drumPads = currentBank.map(element => 
+        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={clickPad}><audio id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
+    );
+    
     return(
         <section className="drum-map">
             <ul>
-                <li id="Heater-1" className="drum-pad" onClick={clickPad}>Q</li>
-                <li id="Heater-2" className="drum-pad" onClick={clickPad}>W</li>
-                <li id="Heater-3" className="drum-pad" onClick={clickPad}>E</li>
-                <li id="Heater-4" className="drum-pad" onClick={clickPad}>A</li>
-                <li id="Clap" className="drum-pad" onClick={clickPad}>S</li>
-                <li id="Open-HH" className="drum-pad" onClick={clickPad}>D</li>
-                <li id="Kick-n'-Hack" className="drum-pad" onClick={clickPad}>Z</li>
-                <li id="Kick" className="drum-pad" onClick={clickPad}>X</li>
-                <li id="Closed-HH" className="drum-pad" onClick={clickPad}>C</li>
+                {drumPads}
             </ul>
         </section>
     );
