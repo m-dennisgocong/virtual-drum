015cad56ee504ba87b3e45af609ad94dabb536fb
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-01 09:37:19 +0700

    close #5 add onClick event and playSound function
    each click on drum-pad element will call the playSound function that trigger the audio element

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -1,14 +1,16 @@
 import './DrumPads.scss';
 const DrumPads = ({currentBank}) => {
 
-    const clickPad = (event) => {
-        console.log("Click!");
+    const playSound = (event) => {
+        const sound = event.target.firstChild;// get the first child which is the audio element
+        sound.currentTime = 0;
+        sound.play();       
     }
-    
+
     const drumPads = currentBank.map(element => 
-        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={clickPad}><audio id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
+        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={playSound}><audio id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
     );
-    
+        
     return(
         <section className="drum-map">
             <ul>
