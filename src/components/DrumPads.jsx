ccbddb178c33cdffe798ce71bff35422225e8006
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-02 13:33:32 +0700

    close #6 add event listener for trigger key
    import useEffect and set a keydown event listener when mount and attach handleKeyDown function that trigger the audio element

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -1,4 +1,5 @@
 import './DrumPads.scss';
+import { useEffect } from 'react';
 const DrumPads = ({currentBank}) => {
 
     const playSound = (event) => {
@@ -7,8 +8,21 @@ const DrumPads = ({currentBank}) => {
         sound.play();       
     }
 
+    const handleKeyDown = (event) => {
+        const sound = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+        sound.currentTime = 0;
+        sound.play();  
+    }
+    
+    useEffect(()=> {
+        document.addEventListener('keydown', handleKeyDown);
+        return() => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    });
+
     const drumPads = currentBank.map(element => 
-        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={playSound}><audio id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
+        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={playSound}><audio data-key={element.keyCode} id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
     );
         
     return(
