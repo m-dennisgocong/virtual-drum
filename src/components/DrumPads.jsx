9ba775448c15947465ffc5238b617a1cb4e818a9
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-04 14:26:21 +0700

    refactor: separate playSound func from click event
    create new function handling onClick event

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -2,19 +2,31 @@ import './DrumPads.scss';
 import { useEffect } from 'react';
 const DrumPads = ({currentBank, displayAudioName}) => {
 
-    const playSound = (event) => {
-        const sound = event.target.firstChild;// get the first child which is the audio element
+
+    const playSound = (sound, soundName) => {
+
         sound.currentTime = 0;
-        sound.play();
-        displayAudioName(event.target.id);       
+        sound.play(); 
+        
+        displayAudioName(soundName); // display the sound audio name when trigger
+
+    }
+
+    const handleClick = (event) => {
+        
+        console.log(currentBank)
+        const sound = event.target.firstChild; // get the first child which is the audio element
+        const soundName = event.target.id; // audio name 
+
+        playSound(sound, soundName); // play the audio
     }
 
     const handleKeyDown = (event) => {
            
         const sound = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-        displayAudioName(sound.parentNode.id)
-        sound.currentTime = 0;
-        sound.play();  
+        const soundName = sound.parentNode.id;
+        
+        playSound(sound, soundName); // play sound
     }
     
     useEffect(()=> {
@@ -25,7 +37,7 @@ const DrumPads = ({currentBank, displayAudioName}) => {
     });
 
     const drumPads = currentBank.map(element => 
-        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={playSound}><audio data-key={element.keyCode} id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
+        <li key={element.keyTrigger} id={element.id} className="drum-pad" onClick={handleClick}><audio data-key={element.keyCode} id={element.keyTrigger} className="clip" src={element.url} />{element.keyTrigger}</li>
     );
         
     return(
