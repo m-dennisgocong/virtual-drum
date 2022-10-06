d73df516013aff9accce7daa1734b8c1a33387e5
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-06 16:36:20 +0700

    add BankPad Component
    create multiple drum pad component with props ( displayAudioName, keyCode, keyTrigger, id and audioUrl )

---
+++ src/components/BankPad.jsx
@@ -0,0 +1,22 @@
+import DrumPads from "./DrumPads";
+import './DrumPad.scss'
+
+const BankPad = ({currentBank, displayAudioName}) => {
+    
+    const drumPads = currentBank.map((element) => {
+        return(
+            <DrumPads
+                key={element.id}
+                displayAudioName={displayAudioName} 
+                keyCode={element.keyCode}
+                keyTrigger={element.keyTrigger}
+                id={element.id}
+                audioUrl={element.url}  
+            />
+        )
+    });
+    return(
+        <div className="pad-bank">{drumPads}</div>
+    );
+}
+export default BankPad;
