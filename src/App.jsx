d8235af8717717b7724d1b05b89c3916366addb6
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-10 12:36:57 +0700

    refactor: remove component & change var/func name
    change state name and function name  to use it for general purpose so I can display any value
    
    remove the ControllerContainer and directly implement Controller Options  to App with out using another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 import './App.scss';
 import BankPad from './components/BankPad';
-import ControlContainer from './components/ControlContainer';
+// import ControlContainer from './components/ControlContainer';
 import {bankOne} from './data/bankOne.js';
 
 
 const App = () => {
 
   const [currentBank, setCurrentBank] = useState(bankOne);
-  const [audioName, setAudioName] = useState("")
+  const [displayString, setdisplayString] = useState(" ")
+  const [volumeValue, setVolumeValue] = useState(5);
 
-  const displayAudioName = (name) => {
-    setAudioName(name);
+  const display = (name) => {
+    setdisplayString(name);
   }
   
   return(
     <main id="drum-machine" className="drum-container">
-      <p id="display">{audioName}</p>
-      <BankPad currentBank={currentBank} displayAudioName={displayAudioName}/>
-      <ControlContainer />
+      <div className="display-container">
+        <p id="display">{displayString}</p>
+      </div>
+      <BankPad currentBank={currentBank} display={display}/>
+      {/* <ControlContainer /> */}
+      <input type="range" name="volume" min={0} max={10} value={volumeValue} onChange={(event)=> setVolumeValue(event.target.value)}/>
+      <div className="switch">
+      </div>
     </main>
   );
 }
