01ae6e9a21926c89d4c831fa6f0b3afec5041241
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-12 13:48:20 +0700

    update: remove drum pads light when power off

--- src/components/DrumPads.jsx
+++ src/components/DrumPads.jsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react';
 const DrumPads = ({keyCode,keyTrigger,id,audioUrl, display, power}) => {
 
     const activateStyle = {
-        backgroundColor: '#333',
-        boxShadow: 'inset 0 0 2em rgb(0, 0, 255, 0.5),inset 0 0 4em #fff, 0 0 2em rgb(0, 0, 255, 0.5), 2px 2px 1em #fff',
-        height: 67.5,
-        top: 3.5
+        backgroundColor: 'rgb(125, 249, 255,.5)',
+        boxShadow: 'inset 0 0 35px rgb(0, 0, 255, 0.5),inset 0 0 55px rgb(255,255,255,.5),inset 0 0 2em rgb(255,20,147,.5), 0 0 20px rgb(0, 0, 255, 0.5), 0 0 10px #fff',
+        height: 65,
+        top: 3,
+        color: "white"
     };
     const inActivateStyle = {
         backgroundColor: '#333',
@@ -14,12 +15,17 @@ const DrumPads = ({keyCode,keyTrigger,id,audioUrl, display, power}) => {
         color: "white"
     };
     const powerOffstyle = {
+        color: 'grey',
         backgroundColor: '#333',
-        height: 67.5,
-        boxShadow: 'unset',
-        top: 3.5
+        boxShadow: '3px 3px 2px rgb(0, 0, 0,0.5)',
     };
-
+    const powerOffClick = {
+        color: 'grey',
+        backgroundColor: '#333',
+        height: '65',
+        boxShadow: '0 0 3px rgb(0, 0, 0,0.5)',
+        top: 3
+    };
     // play the audio
     const playSound = () => {
         if(power){
@@ -30,12 +36,14 @@ const DrumPads = ({keyCode,keyTrigger,id,audioUrl, display, power}) => {
             // display the name of triggered drum pad
             display(id);
             // change style of triggered drum pad  
-            setPadStyle(activateStyle)
-            
-        } else{
-            setPadStyle(powerOffstyle)
+            setPadStyle(activateStyle);
+            setTimeout(() => setPadStyle(inActivateStyle), 100); 
+
+        }else{
+            // click effect for power off
+            setPowerOff(powerOffClick);
+            setTimeout(() => setPowerOff(powerOffstyle), 100);  
         }
-        setTimeout(() => setPadStyle(inActivateStyle), 100);
     }
 
     // onclick
@@ -56,18 +64,20 @@ const DrumPads = ({keyCode,keyTrigger,id,audioUrl, display, power}) => {
 
     
     const [padStyle, setPadStyle] = useState(inActivateStyle);
-
+    const [powerOff, setPowerOff] = useState(powerOffstyle);
     useEffect(()=> {
         document.addEventListener('keydown', handleKeyDown);
         return() => {
             document.removeEventListener('keydown', handleKeyDown);
         };
     });
-        
+
     return(
-        <div className="drum-pad" id={id} onClick={handleClick} style={padStyle}>
+        
+        <div className="drum-pad" id={id} onClick={handleClick} style={power ? padStyle : powerOff}>
             <audio className="clip" id={keyTrigger} src={audioUrl}></audio>{keyTrigger}
         </div>
+
     );
 }
 
